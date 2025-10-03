8d658f0c137b86cb4602cf4f9ec56ecd37688584
Shamim Ahmed Nahid <shamimofficial771@gmail.com>
2025-10-04 03:10:47 +0600

    Add files via upload

---
+++ turtle_virus_pattern.py
@@ -0,0 +1,24 @@
+import turtle as t
+
+screen = t.Screen()
+screen.setup(width = 1.0, height = 1.0)
+
+t.title("Turtle Virus Pattern")
+t.color("red")
+t.bgcolor("black")
+t.speed(0)
+t.pensize(2)
+t.hideturtle()
+t.penup()
+t.goto(0,-100)
+t.pendown()
+
+x = 0 
+i = 0
+while i < 210:
+    t.forward(x)
+    t.left(i)
+    x += 2 
+    i += 1
+
+t.exitonclick()
